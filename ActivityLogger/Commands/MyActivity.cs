d73d5205abca06d89dd9e58a3fe4dc364bbbb3ba
rayzer <82989402+rayzerbrain@users.noreply.github.com>
2021-10-20 13:22:16 -0500

    Add files via upload

---
+++ ActivityLogger/Commands/MyActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Exiled.API.Features;
+using CommandSystem;
+
+namespace ActivityLogger.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    class MyActivity : ICommand
+    {
+        public string Command { get; } = "myactivity";
+
+        public string[] Aliases { get; } = { "ma" };
+
+        public string Description { get; } = "Gets info about your activity on this specific server";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player asker = Player.Get(sender);
+            if(arguments.Count==0)
+            {
+                response = "Error, usage is: .myactivity";
+                return false;
+            }
+            if(PluginMain.Instance.ActivityDict.ContainsKey(asker.UserId))
+            {
+                response = PluginMain.Instance.ActivityDict[asker.UserId].GetInfo();
+                return true;
+            }
+            response = "Your log wasn't found. (Do you have Do Not Track enabled?)";
+            return false;
+        }
+    }
+}
